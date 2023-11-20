7edcb21fedfb51a44f83bc35727011b748b1f711
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-11-20 11:03:30 +0100

    Initial commit

---
+++ src/main/java/EPIC/ENERGY/SERVICE/BEBuildWeek2/BeBuildWeek2Application.java
@@ -0,0 +1,13 @@
+package EPIC.ENERGY.SERVICE.BEBuildWeek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeBuildWeek2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeBuildWeek2Application.class, args);
+	}
+
+}
