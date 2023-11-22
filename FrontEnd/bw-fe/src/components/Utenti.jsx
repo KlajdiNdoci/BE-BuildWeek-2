bfba88395048887a784de53c47b27d3d23c76cbe
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-22 12:45:00 +0100

    aggiunta query frontend

--- FrontEnd/bw-fe/src/components/Utenti.jsx
+++ FrontEnd/bw-fe/src/components/Utenti.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Col, Container, ListGroup, Pagination, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, ListGroup, Modal, Pagination, Row } from "react-bootstrap";
 import Lista from "./Lista";
 import { isDisabled } from "@testing-library/user-event/dist/utils";
 
@@ -8,6 +8,8 @@ const Utenti = () => {
   const [pagina, setPagina] = useState(1);
   const [numPagine, setNumPagine] = useState();
   const [ordine, setOrdine] = useState();
+  const [show, setShow] = useState(false);
+  const [search, setSearch] = useState();
   const getUtenti = async (order, p) => {
     const aut = JSON.parse(localStorage.getItem("token"));
     console.log(aut.accessToken);
@@ -46,6 +48,30 @@ const Utenti = () => {
         const data = await risp.json();
         console.log(data);
         setNumPagine(data.totalPages);
+        console.log(data.content);
+        setListaUtenti(data.content);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleShow = (page) => setShow(true);
+  const handleClose = (page) => setShow(false);
+  const findAllByProvincia = async (page) => {
+    const aut = JSON.parse(localStorage.getItem("token"));
+    try {
+      const risp = await fetch(`http://localhost:3001/clienti/get_all_by_provincia?page=${page - 1}&prov=${search}`, {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${aut.accessToken}`,
+        },
+      });
+      if (risp.ok) {
+        const data = await risp.json();
+        console.log(data);
+        setNumPagine(data.totalPages);
+        setSearch("");
         console.log(data.content);
         setListaUtenti(data.content);
       }
@@ -110,6 +136,9 @@ const Utenti = () => {
               >
                 Provincia sede legale
               </li>
+              <li style={{ cursor: "pointer" }} onClick={handleShow}>
+                cerca provincia
+              </li>
             </ul>
           </Col>
           <Col xs={8} className="d-flex flex-column justify-content-center align-items-center">
@@ -146,6 +175,55 @@ const Utenti = () => {
           </Col>
         </Row>
       </Container>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              findAllByProvincia(1);
+            }}
+          >
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Provincia</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="provincia"
+                autoFocus
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+            </Form.Group>
+            <Modal.Footer>
+              {" "}
+              <Button
+                variant="secondary"
+                onClick={() => {
+                  handleClose();
+                  setSearch("");
+                }}
+              >
+                Close
+              </Button>
+              <Button
+                type="submit"
+                variant="primary"
+                onClick={() => {
+                  handleClose();
+                }}
+                className="ms-3"
+              >
+                Search
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
